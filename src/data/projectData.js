1c4382425d6ade0637bde78cc5f8091fefcc4607
Misha Hoo <hoomisha@gmail.com>
2017-11-08 17:16:02 -0600

    Add project descriptions

--- src/data/projectData.js
+++ src/data/projectData.js
@@ -2,29 +2,27 @@ const projectData = [
   {
     image: './assets/brainstorm.png',
     title: 'Brainstorm',
-    description: `One Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do\
-      eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim\
-      veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo\
-      consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum\
-      dolore eu fugiat nulla pariatur.`,
+    description: `Brainstorm is a team friendly tool that allows users to brainstorm\
+    in real time with friends or co-workers. Sign up, login and invite other users via\
+    email to contribute to a brainstorming session. The clean friendly design is easy\
+    and fun to use.`,
     url: 'https://www.github.com/mishahoo/brainstorm'
   },
   {
     image: './assets/sharemykitchen.png',
     title: 'sharemykitchen',
-    description: `One Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do\
-      eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim\
-      veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo\
-      consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum\
-      dolore eu fugiat nulla pariatur.`,
+    description: `A home sharing application where users can rent and share kitchen spaces\
+    by the day or hour. Create a profile and view beautiful cooking spaces complete with\
+    information on all appliances. Or list your space and share the love.`,
     url: 'https://www.github.com/mishahoo/sharemykitchen'
   },
   {
     image: './assets/Goodfilms.png',
     title: 'Goodfilms',
-    description: `One Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do\
-      eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim\
-      veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo.`,
+    description: `
+      A personal movie database where movie lovers rate, collect, and view information on\
+      watched, favorite, and soon-to-be-watched films. Be your own critic and add reviews\
+      to all your favorite films without ever missing out on what’s new.`,
     url: 'https://www.github.com/mishahoo/jmx2'
   }
 ]
